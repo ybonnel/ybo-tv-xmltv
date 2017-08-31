fc9f7d9e5ce4e6f7e23e21f2c8803d9d5499e77c
Yan Bonnel <ybonnel@gmail.com>
2017-08-31 22:52:06 +0200

    Fix

--- majXmlTv.sh
+++ majXmlTv.sh
@@ -2,12 +2,12 @@ cd /home/ybonnel/ybo-tv/xmltv
 
 echo "`date +"%Y%m%d %T.%N"` : debut grab"
 
-mc2xml -c fr -g 10000 
+./mc2xml -c fr -g 10000 
 
 echo "`date +"%Y%m%d %T.%N"` : fin grab"
 
 
-if [ -f xmltv.xml ]
+if [ -f ./xmltv.xml ]
 then
 	echo "`date +"%Y%m%d %T.%N"` : Lancement de la mise a jour mongo"
 
