1381b0be29d704ed9078c155a2910d3c907ee8a6
Yan Bonnel <ybonnel@gmail.com>
2017-08-31 22:48:16 +0200

    Fix grabber

--- majXmlTv.sh
+++ majXmlTv.sh
@@ -2,20 +2,16 @@ cd /home/ybonnel/ybo-tv/xmltv
 
 echo "`date +"%Y%m%d %T.%N"` : debut grab"
 
-./tv_grab_fr_telerama > xmltv.xml.new
+mc2xml -c fr -g 10000 
 
 echo "`date +"%Y%m%d %T.%N"` : fin grab"
 
 
-if [ -f xmltv.xml.new ]
+if [ -f xmltv.xml ]
 then
-	mv xmltv.xml xmltv.xml.old
-	mv xmltv.xml.new xmltv.xml
-	echo "`date +"%Y%m%d %T.%N"` : Fin mise a jour"
-
-	echo "`date +"%Y%m%d %T.%N"` : Lancement de la mise a jour couchbase"
+	echo "`date +"%Y%m%d %T.%N"` : Lancement de la mise a jour mongo"
 
 	java -jar ybo-tv-helper-jar-with-dependencies.jar xmltv.xml
 
-	echo "`date +"%Y%m%d %T.%N"` : Fin de la mise a jour couchbase"
+	echo "`date +"%Y%m%d %T.%N"` : Fin de la mise a jour mongo"
 fi
