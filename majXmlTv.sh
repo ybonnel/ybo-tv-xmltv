594cfa6ec1e9b651bd1770e517c34dba832347ed
Yan Bonnel <ybonnel@gmail.com>
2017-09-02 23:24:21 +0200

    Nouvelle source de données

--- majXmlTv.sh
+++ majXmlTv.sh
@@ -2,16 +2,7 @@ cd /home/ybonnel/ybo-tv/xmltv
 
 echo "`date +"%Y%m%d %T.%N"` : debut grab"
 
-./mc2xml -c fr -g 10000 
-
-echo "`date +"%Y%m%d %T.%N"` : fin grab"
-
-
-if [ -f ./xmltv.xml ]
-then
-	echo "`date +"%Y%m%d %T.%N"` : Lancement de la mise a jour mongo"
-
-	java -Djavax.xml.accessExternalDTD=all -jar ybo-tv-helper-jar-with-dependencies.jar xmltv.xml
+	java -jar ybo-tv-helper-jar-with-dependencies.jar 
 
 	echo "`date +"%Y%m%d %T.%N"` : Fin de la mise a jour mongo"
 fi
