674cbc9800e42d0dbfe89e8ae850c761df040bb7
ybonnel <ybonnel@gmail.com>
2020-02-11 21:30:08 +0100

    Remove tabs

--- majXmlTv.sh
+++ majXmlTv.sh
@@ -2,6 +2,6 @@ cd /home/ybonnel/ybo-tv/xmltv
 
 echo "`date +"%Y%m%d %T.%N"` : debut grab"
 
-	java -jar ybo-tv-helper-jar-with-dependencies.jar 
+java -jar ybo-tv-helper-jar-with-dependencies.jar 
 
-	echo "`date +"%Y%m%d %T.%N"` : Fin de la mise a jour mongo"
+echo "`date +"%Y%m%d %T.%N"` : Fin de la mise a jour mongo"
